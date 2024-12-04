cd30b0c041e8034eaf3449742094b1e09222f690
Bas van der Hoek <bas@hoek.com>
2024-12-05 00:05:43 +0100

    Use Commander with subcommands to merge into one file

--- README.md
+++ README.md
@@ -3,5 +3,14 @@
 ```sh
 open https://platform.openai.com/settings/profile?tab=api-keys
 export OPENAI_API_KEY=sk-...
-$ deno task run
+$ ./sentence.ts
+Usage: sentence [options] [command]
+A CLI for generating Japanese sentences using OpenAI's GPT-4o
+
+Commands:
+  everyday <word>              An everyday short sentence
+  memorable <word>             A memorable short sentence
+  speech [options] <sentence>  Save sentence as audio
+  help [command]               display help for command
+
 ```
--- sentence.ts
+++ sentence.ts
@@ -1,12 +1,63 @@
+#!/usr/bin/env deno -W=. -E=OPENAI_API_KEY,OPENAI_BASE_URL,OPENAI_ORG_ID,OPENAI_PROJECT_ID,DEBUG -N=api.openai.com:443
+import { Buffer } from "node:buffer";
+import { Command } from "npm:commander";
 import OpenAI from "npm:openai";
 
-const word = Deno.args[0];
 const openai = new OpenAI();
-const completion = await openai.chat.completions.create({
+const complete = async (prompt: string) => {
+  const completion = await openai.chat.completions.create({
     model: "gpt-4o-mini",
     messages: [
-        {"role": "user", "content": `give an everyday short sentence in japanese that uses the word ${word}`}
-    ]
-});
+      { "role": "user", "content": prompt },
+    ],
+  });
 
-console.log(completion.choices[0].message);
+  return completion.choices[0].message.content;
+};
+
+const speech = async (sentence: string, output: string) => {
+  const response = await openai.audio.speech.create({
+    model: "tts-1",
+    voice: "nova",
+    input: sentence,
+  });
+
+  const buffer = Buffer.from(await response.arrayBuffer());
+  await Deno.writeFile(output, buffer);
+};
+
+let cli = new Command();
+cli
+  .description("A CLI for generating Japanese sentences using OpenAI's GPT-4o")
+  .version("0.0.1");
+
+cli.command("everyday")
+  .description("An everyday short sentence")
+  .argument("<word>", "word")
+  .action(async (word) => {
+    const completion = await complete(
+      `Give an everyday short sentence in Japanese that uses the word ${word}`,
+    );
+    console.log(completion);
+  });
+
+cli.command("memorable")
+  .description("A memorable short sentence")
+  .argument("<word>", "word")
+  .action(async (word) => {
+    const completion = await complete(
+      `Give a memorable short sentence in Japanese with the word ${word}`,
+    );
+    console.log(completion);
+  });
+
+cli.command("speech")
+  .description("Save sentence as audio")
+  .argument("<sentence>", "sentence")
+  .requiredOption("-o, --output <file>", "output file")
+  .action(async (sentence, opts) => {
+    await speech(sentence, opts.output);
+    console.log(`Wrote ${sentence} to ${opts.output}`);
+  });
+
+cli.parse();
