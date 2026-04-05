041734c11f05305526bef120097be0cd05ad43a0
Bas van der Hoek <bas@hoek.com>
2026-04-05 16:51:37 +0200

    Log offending xml and error

--- dom.ts
+++ dom.ts
@@ -1,16 +1,20 @@
-import {Document, DOMParser} from "npm:@xmldom/xmldom";
+import { Document, DOMParser } from "npm:@xmldom/xmldom";
 import xpath from "npm:xpath";
 
-export function descriptionList(value: string): Document {
-  let xml: string  = value
-  if(!xml.startsWith("<dl>")) {
+export function descriptionList(value: string): Document | undefined {
+  let xml: string = value;
+  if (!xml.startsWith("<dl>")) {
     xml = `<dl><dt>${xml}</dt></dl>`;
   }
-  console.log(xml);
-  return new DOMParser().parseFromString(xml, "text/xml");
+  try {
+    return new DOMParser().parseFromString(xml, "text/xml");
+  } catch (e) {
+    console.error(xml, e.message || e);
+    return undefined;
+  }
 }
 
 export function textContent(exp: string, doc: Document): string {
   const nodes: [Node] = xpath.select(exp, doc);
-  return nodes.map((node) => node.textContent)[0] ?? "";
+  return nodes.map((node) => node.textContent)[0]?.trim() ?? "";
 }
