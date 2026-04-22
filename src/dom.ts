d33a2c2bb11d4bd5b588d270762dd11b891f5298
Bas van der Hoek <bas@hoek.com>
2026-04-22 20:25:56 +0200

    Fix warnings

--- src/dom.ts
+++ src/dom.ts
@@ -10,19 +10,19 @@ export function dl(value: string): string {
 
 export function extractXPaths(xml: string, keys: any): any | undefined {
   try {
-    const doc =  new DOMParser().parseFromString(xml, "text/xml");
+    const doc = new DOMParser().parseFromString(xml, "text/xml");
     const result = {};
     for (const [key, value] of Object.entries(keys)) {
-      Object.assign(result, { [key]: textContent(value, doc) });
+      Object.assign(result, { [key]: textContent(<string>value, doc) });
     }
     return result;
-  } catch (e) {
-    console.error(xml, e.message || e);
+  } catch {
+    console.error("Couldn't parse", xml);
     return undefined;
   }
 }
 
 export function textContent(exp: string, doc: Document): string {
-  const nodes: [Node] = xpath.select(exp, doc);
+  const nodes: [Node] = xpath.select(exp, doc) as [Node];
   return nodes.map((node) => node.textContent)[0]?.trim() ?? "";
 }
